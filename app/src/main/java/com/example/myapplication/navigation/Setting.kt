d621ac2c01759eb67a991f3d0aa21a9de5a1e77e
lexxuanduc1 <lexuanduc147@gmail.com>
2024-03-20 07:54:59 +0700

    change password

--- app/src/main/java/com/example/myapplication/navigation/Setting.kt
+++ app/src/main/java/com/example/myapplication/navigation/Setting.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.navigation
 
 import android.graphics.drawable.Icon
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -23,6 +24,7 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.ErrorOutline
@@ -33,23 +35,56 @@ import androidx.compose.material.icons.rounded.Password
 import androidx.compose.material.icons.rounded.Sync
 import androidx.compose.material.icons.rounded.WbSunny
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.myapplication.composable.CustomDialog
 import com.example.myapplication.composable.DashBoardPreview
+import com.example.myapplication.composable.removeLocalGoal
 import com.example.myapplication.repositories.user.LocalDataUser
+import com.example.myapplication.repositories.user.UserRepo
 import com.example.myapplication.utils.ColorUtils
 import com.example.myapplication.utils.Navigation
 import com.example.myapplication.utils.TextSizeUtils
+interface ChangePassCallback {
+    fun onSuccess(result:String)
+    fun onError(errormessage: String)
+}
+fun onChangePassword(token:String,userid:String,oldpassword:String,newpassword:String,callback: ChangePassCallback){
+    UserRepo.getInstance().ChangePassword(token,userid,oldpassword,newpassword){
+        res,err ->run{
+            if(err!=null){
+                callback.onError("Đã  xảy ra lỗi!!!")
+            }
+        else{
+            callback.onSuccess(res?.result.toString())
+            }
+    }
+    }
+}
+fun checkvalidatepass(oldpassword: String,newpassword: String):String{
+    if(oldpassword.isBlank()||newpassword.isBlank()) return "Không được để trống !!"
+    if(oldpassword.length<6) return "Mật kẩu cũ quá ngắn!!"
+    if(newpassword.length<6) return "Mật kẩu mới quá ngắn!!"
+    if(newpassword==oldpassword) return "Mật khẩu mới không được trùng mới mật khẩu cũ!"
+    return ""
+}
 
-@Composable
+@Composable()
 fun ItemSetting( name:String,icon: ImageVector,onClick: () -> Unit,color:Color= Color(ColorUtils.accent)){
     Button(
         onClick = onClick,
@@ -86,6 +121,19 @@ fun SettingPage(navController: NavController) {
     val context= LocalContext.current
     val userLocal = LocalDataUser(context)
     val userInfor = userLocal.getUser()
+    var errorMessage by remember { mutableStateOf("") }
+    var loading by remember {
+        mutableStateOf(false)
+    }
+    var oldPassword by remember {
+        mutableStateOf("")
+    }
+    var newPassword by remember {
+        mutableStateOf("")
+    }
+    var showdialog by remember {
+        mutableStateOf(false)
+    }
     Surface(
         color= Color(ColorUtils.background)
     ) {
@@ -127,7 +175,9 @@ fun SettingPage(navController: NavController) {
 
                             )
                         ItemSetting(name = "Xem thông tin ", icon = Icons.Rounded.AccountCircle, onClick = {null})
-                        ItemSetting(name = "Đổi mật khẩu", icon = Icons.Rounded.Password, onClick = {})
+                        ItemSetting(name = "Đổi mật khẩu", icon = Icons.Rounded.Password, onClick = {
+                            showdialog=true
+                        })
 
                         ItemSetting(
                             name= "Đăng xuất"
@@ -162,6 +212,118 @@ fun SettingPage(navController: NavController) {
                 }
             }
         }
+    }
+    if(showdialog){
+
+       CustomDialog(
+           onDismissRequest = {
+               showdialog=false
+           },
+           title = "Đổi mật khẩu",
+           subtitle = "",
+           Body = {
+               Column(
+                   modifier = Modifier
+                       .padding(40.dp),
+                   horizontalAlignment = Alignment.CenterHorizontally,
+                   verticalArrangement = Arrangement.Top
+               ) {
+                   TextField(
+                       value = oldPassword,
+                       onValueChange = { oldPassword = it },
+                       label = { Text(text = "Mật khẩu cũ") },
+                       modifier = Modifier
+                           .clip(shape = RoundedCornerShape(8.dp))
+                           .background(Color.White)
+                           .fillMaxWidth(),
+                       textStyle = TextStyle(fontSize = TextSizeUtils.MEDIUM),
+                       visualTransformation = PasswordVisualTransformation(),
+                       singleLine = true
+
+                   )
+                   TextField(
+                       value = newPassword,
+                       onValueChange = { newPassword = it },
+                       label = { Text(text = "Mật khẩu mới") },
+                       modifier = Modifier
+                           .padding(bottom = 10.dp, top = 30.dp)
+                           .clip(shape = RoundedCornerShape(8.dp))
+                           .background(Color.White)
+                           .fillMaxWidth(),
+                       textStyle = TextStyle(fontSize = TextSizeUtils.MEDIUM),
+                       visualTransformation = PasswordVisualTransformation(),
+                       singleLine = true
+
+                   )
+                   if (errorMessage.isNotEmpty()) {
+                       Text(
+                           text = errorMessage,
+                           color = Color.Red,
+                           fontSize = TextSizeUtils.SMALL,
+                           modifier = Modifier.padding(top = 8.dp)
+                       )
+                   }
+               }
+           },
+           DeclineBtn = {
+               Button(
+                   onClick = {
+                             showdialog=false
+                   },
+                   modifier = Modifier
+                       .fillMaxWidth(),
+                   colors = ButtonDefaults.buttonColors(
+                       backgroundColor = Color.Transparent,
+                       contentColor = Color.Gray
+                   )
+                   ) {
+                   Text(text = "Hủy")
+               }
+           },
+           ConfirmBtn = {
+               Button(
+                   onClick = {
+
+                       errorMessage = checkvalidatepass(oldPassword, newPassword)
+                       loading = true
+
+                       val userid=userInfor?._id.toString()
+                       val token=userLocal.getToken().toString()
+                       Log.d("App",userid)
+                       Log.d("App",token)
+                       if(errorMessage==""){
+
+                           onChangePassword("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWUxZWQ0ZjljMDA1NWI3NmFjNDUwNGMiLCJ1c2VybmFtZSI6InRlc3QiLCJwYXNzd29yZCI6IiQyYiQxMCR1S1AxV2kyYnhBbG1PaGZTbW5PbmYuR0c5eDZibERzeHZvYlJVZUwzYjRheFRQT1pSY055VyIsIl9fdiI6MCwiaWF0IjoxNzEwNzc3NDgzfQ.Q3dtDI_ZeM8sjY6_MAwz00meux6cohoPrpOLiOpy0ww",userid,oldPassword,newPassword,object :ChangePassCallback{
+                               override fun onSuccess(result: String) {
+
+                                   Toast.makeText(context, "Đổi mật khẩu thành công!!!", Toast.LENGTH_SHORT).show()
+                               }
+
+                               override fun onError(errormessage: String) {
+
+                                   errorMessage=errormessage
+                                   loading = false
+                               }
+
+                           })
+                       }
+                       else{
+
+                           loading=false
+                       }
+
+                   },
+                   modifier = Modifier
+                       .fillMaxWidth(),
+                   colors = ButtonDefaults.buttonColors(
+                       backgroundColor = Color(ColorUtils.primary)
+                   ),
+                   enabled = !loading
+               ) {
+                  Text(text = if(loading) "Đang xử lý" else "Đổi mật khẩu")
+               }
+           }
+       )
     }
 
 }
